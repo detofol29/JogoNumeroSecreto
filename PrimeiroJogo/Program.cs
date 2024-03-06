bb65c8fee8683d0c248acb5a243251619c8b568f
User <User@DESKTOP-5C2E3UU>
2024-03-05 21:19:55 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroJogo/Program.cs
@@ -0,0 +1,22 @@
+﻿Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 100);
+////
+do{
+    Console.WriteLine("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine());
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns, você acertou o número!");
+        break;
+    }else if(chute < numeroSecreto)
+    {
+        Console.WriteLine("O número é menor");
+    }
+    else
+    {
+        Console.WriteLine("O número é maior");
+    }
+}while(true);
+
+
