41d166cdee3166d808c47475a4d959cf07204a7c
User <User@DESKTOP-5C2E3UU>
2024-03-06 00:14:03 -0300

    Criação do número de rodadas e a limpeza do console.

--- PrimeiroJogo/Program.cs
+++ PrimeiroJogo/Program.cs
@@ -1,21 +1,27 @@
 ﻿Random numeroAleatorio = new Random();
 int numeroSecreto = numeroAleatorio.Next(1, 100);
-////
+int rodadas = 1;
 do{
     Console.WriteLine("Digite um número entre 1 e 100: ");
     int chute = int.Parse(Console.ReadLine());
 
     if (chute == numeroSecreto)
     {
+        
         Console.WriteLine("Parabéns, você acertou o número!");
+        Console.WriteLine($"Acertou com {rodadas} tentativas.");
         break;
     }else if(chute < numeroSecreto)
     {
-        Console.WriteLine("O número é menor");
+        Console.Clear();
+        Console.WriteLine("O número secreto é maior");
+        rodadas++;
     }
     else
     {
-        Console.WriteLine("O número é maior");
+        Console.Clear();
+        Console.WriteLine("O número secreto é menor");
+        rodadas++;
     }
 }while(true);
 
